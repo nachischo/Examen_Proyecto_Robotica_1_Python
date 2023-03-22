cb819f7dcf13cf7a4f5855d22979fb77f95e0ede
nachischo <nachodriver8@gmail.com>
2023-03-22 10:30:53 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -1,2 +1,12 @@
-def read_data():
-    
+import csv
+
+def read_data(nombreFicheroCSV):
+
+    allDataInCSV = csv.reader(nombreFicheroCSV)
+    diccionarioRes = {}
+    numeroClave = 1
+
+    for row in allDataInCSV:
+        diccionarioRes["dato"+str(numeroClave)]
+
+    return diccionarioRes
