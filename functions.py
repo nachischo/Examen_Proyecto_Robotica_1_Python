81a6d5a650370bf5a53a555bc7eea673b6bd2f30
nachischo <nachodriver8@gmail.com>
2023-03-22 11:12:58 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+#------------Autor: Ignacio Miguel Sandalinas García------------
+#-----------------------Fecha: 22.03.2023-----------------------
+
 import csv
 
 def read_data(nombreFicheroCSV):
@@ -8,12 +11,28 @@ def read_data(nombreFicheroCSV):
     with open(nombreFicheroCSV, 'r') as ficheroCSV:
         allDataInCSV = csv.reader(ficheroCSV)
         for row in allDataInCSV:
-            diccionarioRes["dato"+str(numeroClave)]={"type":row[0],"fixed acidity":row[1],"volatile acidity":row[2],"citric acid":row[3],"type":row[4]}
-            numeroClave+=1
+            estanTododsLosDatos = True
+        
+            for dataPiece in row:
+                if dataPiece == "":
+                    estanTododsLosDatos=False
 
-    
-    
+            if estanTododsLosDatos == True:
+                diccionarioRes["dato"+str(numeroClave)]={"type":row[0],"fixed acidity":row[1],"volatile acidity":row[2],"citric acid":row[3],"residual sugar":row[4],"chlorides":row[5],"free sulfur dioxide":row[6],"total sulfur dioxide":row[7],"density":row[8],"PH":row[9],"sulphates":row[10],"alcohol":row[11],"quality":row[12]}
+                numeroClave+=1
 
-    
+    if numeroClave<11:
+        raise ValueError("Ha ocurrido la excepción ValueError")
 
     return diccionarioRes
+
+def split(wineDataDictionary):
+    diccionarioWhiteRes = {}
+    diccionarioRedRes = {}
+
+    for wine in wineDataDictionary:
+        if wine["type"]=="white":
+            diccionarioWhiteRes[] = {}
+
+
+    return diccionarioWhiteRes, diccionarioRedRes
